3a9d696b8405cd6786f39593e9476abde98ba23f
helltractor <1664309610@qq.com>
2024-06-07 11:25:11 +0800

    Update changes

--- Math/perm.py
+++ Math/perm.py
@@ -2,6 +2,7 @@
 # @File : perm.py
 # @Time : 2024/5/13 下午10:32
 # @Author : Helltractor
+# @Description : Permutation number
 
 # mod = pow(10, 9) + 7
 # l = 3 * pow(10, 5) + 5
@@ -15,14 +16,16 @@
 # 	return fact[n] * inv[n - r] % mod if n >= r >= 0 else 0
 
 class Perm:
-	
-	def __init__(self):
-		self.factorial = [1] * (l + 1)
-		self.inverse = [1] * (l + 1)
-		self.inverse[l] = pow(self.factorial[l], mod - 2, mod)
-		for i in range(l):
-			self.factorial[i + 1] = i * self.factorial[i] % mod
-			self.inverse[l - i - 1] = self.inverse[l - i] * (l - i) % mod
-		
-	def perm(self, n, r):
-		return self.factorial[n] * self.inverse[n - r] % mod if n >= r >= 0 else 0
+    
+    def __init__(self):
+        self.mod = pow(10, 9) + 7
+        self.l = 3 * pow(10, 5) + 5
+        self.fact = [1] * (self.l + 1)
+        self.inv = [1] * (self.l + 1)
+        self.inv[self.l] = pow(self.fact[self.l], self.mod - 2, self.mod)
+        for i in range(self.l):
+            self.fact[i + 1] = i * self.fact[i] % self.mod
+            self.inv[self.l - i - 1] = self.inv[self.l - i] * (self.l - i) % self.mod
+            
+    def perm(self, n, r):
+        return self.fact[n] * self.inv[n - r] % self.mod if n >= r >= 0 else 0
